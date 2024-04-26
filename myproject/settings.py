9b89189e3a86c8ab1f740f8b56a2b3e6a48fdd99
Tastan <tastanbay02@gmail.com>
2024-04-26 13:50:24 +0600

    23

--- myproject/settings.py
+++ myproject/settings.py
@@ -85,26 +85,26 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ospan',
-        'USER': 'ospan',
-        'PASSWORD': 'ospan',
-        'HOST': '185.129.49.37',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ospan',
+#         'USER': 'ospan',
+#         'PASSWORD': 'ospan',
+#         'HOST': '185.129.49.37',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 
 
 # Password validation
