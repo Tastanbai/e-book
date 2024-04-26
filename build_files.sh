08ff0a8f264701ddc03bef968228209df5b66fe0
Tastan <tastanbay02@gmail.com>
2024-04-26 12:00:52 +0600

    add vercel

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+#!/bin/bash
+echo "Where is Python:"
 which python
+echo "Python Version:"
+python --version
+echo "Where is Pip:"
 which pip
+echo "Pip Version:"
+pip --version
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
