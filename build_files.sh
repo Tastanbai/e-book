52caa1765971f8abde0529f3116582f9cddc4908
Tastan <tastanbay02@gmail.com>
2024-04-26 13:04:33 +0600

    as

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-export PATH="/usr/bin/python:$PATH"
-export PATH="$HOME/.local/bin:$PATH"
-which python
-which python3
 
-# Проверка на наличие файла requirements.txt перед установкой зависимостей
-if [ -f "requirements.txt" ]; then
-    pip install -r requirements.txt
-else
-    echo "requirements.txt not found"
-fi
+python -m pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
 
-# Проверка на наличие manage.py перед выполнением Django команд
-if [ -f "manage.py" ]; then
-    python manage.py collectstatic --no-input
-else
-    echo "manage.py not found"
-fi
+python manage.py collectstatic
